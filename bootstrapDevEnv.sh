87dffe9701ab02bafffd658a3f963cbcab1bd33b
wmalyk28 <49351814+wmalyk28@users.noreply.github.com>
2022-05-24 09:39:19 -0400

    Update bootstrapDevEnv.sh

--- bootstrapDevEnv.sh
+++ bootstrapDevEnv.sh
@@ -2,6 +2,6 @@ cd ~
 mkdir devSpace
 git config --global user.email "william.malyk@gmail.com"
 git config --global user.name "William Malyk"
-gred=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
+export gred=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
 
 echo "Dev Env Setup"
